bfd3bd9ecc2cc6706b2ab04e04b0275c924f0f73
andrewphan865@gmail.com <andrewphan865@gmail.com>
2024-08-28 18:04:03 +0930

    add homes-app tutotial

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-log" src="/assets/logo.svg" alt="logo" aria-hidden="true">       
+      </header>
+    </main>    
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Homes App';
+}
